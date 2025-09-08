c606a32e556953d9be4d6991afeaf494e2a556b5
SHANMUK026 <shhannu02@gmail.com>
2025-09-08 10:52:57 +0530

    final commit

--- LoanEligibilityChecker/src/main/java/com/example/LoanEligibilityChecker/Entity/LenderRules.java
+++ LoanEligibilityChecker/src/main/java/com/example/LoanEligibilityChecker/Entity/LenderRules.java
@@ -1,6 +1,5 @@
 package com.example.LoanEligibilityChecker.Entity;
 
-import com.example.LoanEligibilityChecker.Dto.LenderRulesResponseDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
